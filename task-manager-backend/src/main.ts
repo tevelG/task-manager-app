df7307aba5f51c5bfd164312ae708466cd8f49d8
Tevel Galipoliti <tevelgalipoliti@gmail.com>
2024-10-19 22:36:04 +0300

    added anable cors

--- task-manager-backend/src/main.ts
+++ task-manager-backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:5173',  // Allow the frontend URL
+    methods: 'GET,POST,PUT,DELETE',  // Specify the allowed HTTP methods
+    credentials: true,  // If you are sending cookies or authentication tokens
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
